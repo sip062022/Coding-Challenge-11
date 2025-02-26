ba7ab8dc2538cead5d43f8b809e4b722ad3cdce2
Sahara Sipple <sahara36@usf.edu>
2025-02-25 21:41:37 -0500

    Task 2 - Created Borrower Class

--- cc_11.js
+++ cc_11.js
@@ -8,11 +8,11 @@ class Book { // establish class "Book"
         this.copies = copies;
     }
 
-    getDetails () { // defines method getDetails
+    getDetails() { // defines method getDetails
         return `Title: ${this.title}, Author: ${this.author}, ISBN: ${this.isbn}, Copies: ${this.copies}`; // returns formatted string
     }
 
-    updateCopies (quantity) { // defines method updateCopies
+    updateCopies(quantity) { // defines method updateCopies
         this.copies += quantity; // adds quantity when book is returned or borrowed
     }
 }
@@ -22,3 +22,31 @@ console.log(book1.getDetails()); // Expected output: "Title: The Great Gatsby", 
 
 book1.updateCopies(-1); // Removes 1 copy from quantity
 console.log(book1.getDetails()); // Expected output: "Title: The Great Gatsby", Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 4"
+
+// Task 2: Creating a Borrower Class //
+
+class Borrower { // establishes class "Borrower"
+    constructor (name, borrowerId, borrowedBooks) { // defines properties of the class
+        this.name = name;
+        this.borrowerId = borrowerId;
+        this.borrowedBooks = [];
+    }
+
+    borrowBook(book) { // adds method borrowBook
+        this.borrowedBooks.push(book); // adds the book to the borrowed books array
+    }
+
+    returnBook(book) { // adds method returnBook
+        const index = this.borrowedBooks.indexOf(book); // finds the book
+        if (index !== -1) { // finds matching book
+            this.borrowedBooks.splice(index, 1); // removes that matching book from the array
+        }
+    }
+}
+
+const borrower1 = new Borrower("Alice Johnson", 201); // adds new borrower instance
+borrower1.borrowBook("The Great Gatsby"); // adds The Great Gatsby to the borrowed book array
+console.log(borrower1.borrowedBooks); // Expected output: ["The Great Gatsby"]
+
+borrower1.returnBook("The Great Gatsby") // removes book from borrowed book array
+console.log(borrower1.borrowedBooks); // Expected output: []
